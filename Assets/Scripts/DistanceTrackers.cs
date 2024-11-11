d0a444ffa2481b34d5eb23207a0441492655acd3
NotYourMom44 <esihleluzukoribisi@gmail.com>
2024-11-11 11:40:20 +0200

    Assets and distance
    Added new assets for the houses along with some other buildings. Added a distance travelled system on the UI to show how far the truck travels

---
+++ Assets/Scripts/DistanceTrackers.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DistanceTrackers : MonoBehaviour
+{
+
+    public TMP_Text DistanceText;
+    private Vector3 startPosition;
+    private float totalDistance = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalDistance = Vector3.Distance(startPosition, transform.position);
+
+        DistanceText.text = "Distance : " + totalDistance.ToString("F2") + "m";
+    }
+}
